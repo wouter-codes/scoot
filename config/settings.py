277eecd53d79be9c5941d430c50d84b4b1714ba7
wouter-codes <wouter.klinkenberg@outlook.com>
2026-02-17 17:27:19 +0000

    Add templates directory, create index template, and update base HTML with navigation

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'): # This file does not exist on the deployed version
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +79,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
