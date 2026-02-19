ad561c968aff797fa74fef1a9dffdbd6e09343b4
wouter-codes <wouter.klinkenberg@outlook.com>
2026-02-19 20:57:15 +0000

    Enhance RideSearchForm with placeholders and label adjustments for improved usability

--- rides/forms.py
+++ rides/forms.py
@@ -16,6 +16,19 @@ class RideSearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['date'].required = False
+        
+        # Set placeholders and hide labels
+        self.fields['origin'].widget.attrs['placeholder'] = 'Leaving from'
+        self.fields['destination'].widget.attrs['placeholder'] = 'Going to'
+        self.fields['date'].widget.attrs['placeholder'] = 'On'
+        self.fields['min_passengers'].widget.attrs['placeholder'] = 'Passengers'
+        
+        # Hide labels
+        self.fields['origin'].label = ''
+        self.fields['destination'].label = ''
+        self.fields['date'].label = ''
+        self.fields['min_passengers'].label = ''
+        
         self.helper = FormHelper()
         self.helper.form_method = 'get'
         self.helper.layout = Row(
@@ -23,15 +36,12 @@ class RideSearchForm(forms.ModelForm):
             Column('destination', css_class='col-12 col-lg-3'),
             Column('date', css_class='col-12 col-lg-2'),
             Column('min_passengers', css_class='col-12 col-lg-2'),
-            Column(Submit('submit', 'Search', css_class='btn-signup w-100 my-3'), css_class='col-12 col-lg-2 d-flex align-items-end'),
+            Column(Submit('submit', 'Search', css_class='btn-signup w-100'), css_class='col-12 col-lg-2'),
         )
     
     class Meta:
         model = Rides
         fields = ['origin', 'destination', 'date']
-        labels = { 'origin': 'Leaving from',
-                   'destination': 'Going to',
-                   'date': 'On' }
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'})
         }
