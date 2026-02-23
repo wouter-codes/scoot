9ed0c75be4a99ea1207dac769c572da5e5c75fc0
wouter-codes <wouter.klinkenberg@outlook.com>
2026-02-23 23:23:48 +0000

    Add About page and corresponding view; remove commented code from views

--- rides/urls.py
+++ rides/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('ride-request/<int:request_id>/edit/', views.edit_ride_request, name='edit_ride_request'),
     path('ride-request/<int:request_id>/approve/', views.approve_ride_request, name='approve_ride_request'),
     path('ride-request/<int:request_id>/reject/', views.reject_ride_request, name='reject_ride_request'),
+    path('about/', views.about, name='about'),
 ]
--- rides/views.py
+++ rides/views.py
@@ -351,22 +351,6 @@ def reject_ride_request(request, request_id):
     # If GET, show a confirmation page (optional, not implemented here)
     return redirect('ride_detail', ride_id=ride.id)
 
-# Create your views here.
-# class PostList(generic.ListView):
-#     """
-#     Returns all published rides in :model:`rides.Rides`
-#     and displays them in a page of six posts. 
-#     **Context**
-
-#     ``queryset``
-#         All published instances of :model:`rides.Rides`
-#     ``paginate_by``
-#         Number of posts per page.
-        
-#     **Template:**
-
-#     :template:`rides/index.html`
-#     """
-#     queryset = Rides.objects.filter(seats_available__gt=0)
-#     template_name = "rides/index.html"
-#     paginate_by = 6
+def about(request):
+    """Render the About page."""
+    return render(request, 'about.html')
